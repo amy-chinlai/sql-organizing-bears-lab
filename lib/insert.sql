2c91e8f4943300a8ff0bc360f6abc19d4aea3f5a
amy-chinlai <amy@actionnetwork.org>
2021-08-17 15:20:51 -0400

    commit tree 3d2780956ddf15c5c1b385e77396ad9c3dcb30e2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'brown', 'hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'black', 'crazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 41, 'F', 'brown', 'rude', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 13, 'M', 'black', 'strict', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 35, 'F', 'polar', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'polar', 'mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 19, 'F', 'black', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, 'M', 'brown', 'hungry', 1);
